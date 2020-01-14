93d45371d8857b9fb16720e920671ef47278936b
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2020-01-14 12:42:08 +0100

    Initial commit.

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('Example test', () => {
+  const [a, b] = [0, 1];
+  expect(a === b).toBeFalsy();
+});
